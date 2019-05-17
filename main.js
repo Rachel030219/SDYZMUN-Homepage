85ac4d43de0ea4d7de5020f1b8c4a1cda04defa3
Rachel030219 <tangrui003@gmail.com>
2019-05-17 21:25:15 +0800

    redesigned

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-var archivesBackground = document.getElementsByClassName("main-archives-transitionBG")[0];
-var blogEntrance = document.getElementsByClassName("main-archives-text")[0];
+// To avoid incomplete loading
+window.onload=function(){
+    var qrDialog = document.getElementById('QQDialog');
+    qrDialog.addEventListener('confirm.mdui.dialog', function () {
+        copyToClipboard("3529609351");
+    })
+};
 
 function onBlogMouseOver() {
     var archivesBackground = document.getElementsByClassName("main-archives-transitionBG")[0];
@@ -8,4 +13,57 @@ function onBlogMouseOver() {
 function onBlogMouseOut() {
     var archivesBackground = document.getElementsByClassName("main-archives-transitionBG")[0];
     archivesBackground.id = "";
+}
+
+// Here comes click events
+function onBlogClick () {
+    window.location.href = "https://blog.sdyzmun.club";
+}
+function onGitHubClick () {
+    window.location.href = "https://github.com/SDYZMUN";
+}
+function onEmailClick () {
+    window.location.href = "mailto:sdyzmun@qq.com";
+}
+
+function copyToClipboard(text) {
+    var textArea = document.createElement("textarea");
+  
+    // Place in top-left corner of screen regardless of scroll position.
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+  
+    // Ensure it has a small width and height. Setting to 1px / 1em
+    // doesn't work as this gives a negative w/h on some browsers.
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+  
+    // We don't need padding, reducing the size if it does flash render.
+    textArea.style.padding = 0;
+  
+    // Clean up any borders.
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+  
+    // Avoid flash of white box if rendered for any reason.
+    textArea.style.background = 'transparent';
+  
+  
+    textArea.value = text;
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
+  
+    document.body.removeChild(textArea);
 }
